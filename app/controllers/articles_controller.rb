d01523632dbcc659b3ad4b760db2c72af251d7fc
Ives Laurent <ives.laurent@kokorio.com>
2019-03-12 12:04:27 -0600

    fix create, add flash and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,24 @@
 class ArticlesController < ApplicationController
+  def index
+  end
+
   def new
     @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to articles_path(@article)
+    else
+      render :new
+    end
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
 
 
 
