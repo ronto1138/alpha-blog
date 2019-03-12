8278ddbdbdaaf17e9f3d77bce326f266c59c1bc9
Ives Laurent <ives.laurent@kokorio.com>
2019-03-12 13:37:19 -0600

    Edit, update, show index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def new
@@ -20,7 +21,19 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
 
   private
     def article_params
