4ec0b17341c7e4b7f1ef273fe1b5d7f72c90afa1
Ives Laurent <ives.laurent@kokorio.com>
2019-04-11 18:02:02 -0500

    add user listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+
   def new
     @user = User.new
   end
@@ -27,6 +32,12 @@ class UsersController < ApplicationController
       rendenr 'edit'
     end      
   end
+
+  def show
+    @user = User.find(params[:id])
+    
+  end
+
 
   private
   def user_params
