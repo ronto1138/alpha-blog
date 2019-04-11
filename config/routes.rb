6efcf0dc8ec917cc5fa0728aba9960e97ce99e10
Ives Laurent <ives.laurent@kokorio.com>
2019-04-11 13:05:37 -0500

    Add signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'about', to: 'wellcome#about'
 
   resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 
+#  post "users", to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get 'pages/home', to: 'pages#home'
 end
