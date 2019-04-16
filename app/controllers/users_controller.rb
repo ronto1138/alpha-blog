57db83a8f3f42efbb439c02f222d78d64a580a3d
Ives Laurent <ives.laurent@kokorio.com>
2019-04-16 12:41:49 -0500

    User authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.paginate(page: params[:page], per_page:5)
   end
@@ -19,22 +20,19 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-    
+        
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
     else
-      rendenr 'edit'
+      render 'edit'
     end      
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page:5)
   end
 
@@ -43,6 +41,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 
 end
 
+  
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "signup", to: "users#new"
   resources :users, except: [:new]
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
 #  post "users", to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get 'pages/home', to: 'pages#home'
