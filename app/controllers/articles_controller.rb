2c7711b397fc58d554510b36a36d7eae9ae83cf6
Ives Laurent <ives.laurent@kokorio.com>
2019-04-30 09:25:36 -0500

    Admin user funtionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,4 +54,10 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :description)
     end
 
+    def require_same_user
+      if current_user != @article.user && !current_user.admin?
+        flash[:danger] = "You can only edit your own articles"
+        redirect_to root_path
+    end
+
 end
