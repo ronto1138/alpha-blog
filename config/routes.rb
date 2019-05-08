19a3f05afd030ded7f9dabb8f5f4f7236c421249
Ives Laurent <ives.laurent@kokorio.com>
2019-05-08 10:27:20 -0500

    add categories model and test

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'login', to: 'sessions#destroy'
-#  post "users", to: "users#create"
+  
+  resources :categories, except: [:destroy]
+
+  #  post "users", to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get 'pages/home', to: 'pages#home'
 end
--- test/controllers/wellcome_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class WellcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get wellcome_home_url
-    assert_response :success
-  end
-
-end
